7e0e8a228c3bb67e3440a5300f5916bba7dab749
Margoshech <margoshech@gmail.com>
2023-04-23 05:53:13 +0300

    Seminar

---
+++ Seminar_sishar1/Program.cs
@@ -0,0 +1,106 @@
+﻿// Console.WriteLine("Введите целоче число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int result = num * num;
+// Console.WriteLine(result);
+
+// if (num > 0)
+// {
+    // Console.WriteLine("Было введено положительное число");
+// }
+// else if (num == 0)
+    // Console.WriteLine("Был введен ноль");
+// else
+    // Console.WriteLine("Было введено отрицательное число");
+
+
+
+
+
+// Console.WriteLine("Введите первое число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// int result = num;
+// Console.WriteLine(result);
+// if (num == 0 || num2 == 0) {
+//     Console.WriteLine("Был введен ноль");
+// } else if (num == num2*num2) {
+//     Console.WriteLine("является квадратом");
+// }
+// else
+//     Console.WriteLine("не является квадратом");
+
+
+
+
+
+// Console.WriteLine("Введите число от 1 до 7");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num == 1) {
+// Console.WriteLine("Понедельник");
+// }
+// else if (num ==2) {
+//     Console.WriteLine("Вторник");
+// }
+// else if (num == 3) {
+// Console.WriteLine("Среда");
+// }
+// else if (num == 4) {
+// Console.WriteLine("Четверг");
+// }
+// else if (num == 5) {
+// Console.WriteLine("Пятница");
+// }
+// else if (num == 6) {
+// Console.WriteLine("Суббота");
+// }
+// else if (num == 7) {
+// Console.WriteLine("Воскресенье");
+// }
+// else
+// Console.WriteLine("ошибка, введите число от 1 до 7");
+
+
+// Console.WriteLine("Введите целое число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num <= 0) {
+//     Console.WriteLine("Введите целое положительное число");
+//     System.Environment.Exit(1);
+// } 
+// int start = num * (-1);
+
+// for (int i = start; i <= num; i++) {
+//     Console.WriteLine(i);
+// } 
+// int i = start;
+// while (i <= num) {
+//     Console.WriteLine(i);
+//     i++;
+// }
+
+
+
+// Console.WriteLine("Введите трехзначное число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num > 99  && num < 1000)
+// {
+//     Console.WriteLine(num%10);
+// }
+// else{
+//     Console.WriteLine("ошибка,введите трехзначное число");
+// }
+
+
+
+
+Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 99)
+{
+    while (num >= 1000) {
+        num = num / 10;
+    }
+
+    Console.WriteLine(num % 10);
+}
