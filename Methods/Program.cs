c4d61b6a54c32b7337cc04e4d63257b205f24769
Margoshech <margoshech@gmail.com>
2023-05-21 04:25:39 +0300

    seminar4s

---
+++ Methods/Program.cs
@@ -0,0 +1,110 @@
+﻿// string Metod4(int count, string text)
+// {
+// int i = 0;
+// string result = String.Empty;
+// while (i<count)
+// {
+// result = result + text;
+// i++;
+// }
+// return result;
+// }
+// string res = Metod4(10, "asdf");
+// Console.WriteLine(res);
+
+
+
+// string Metod4(int count, string text)
+// {
+// string result = String.Empty;
+// for (int i = 0; i<count; i++) 
+// // - вначале ключевое слово, затем инициализация
+// // счётчика, после проверка условия и инкремент (увеличение счётчика).
+// {
+// result = result + text;
+// }
+// return result;
+// }
+// string res = Metod4(10, "asdf");
+// Console.WriteLine(res);
+
+
+
+// string Replase(string text, char oldValue, char newValue)
+// {
+// string result = String.Empty;
+// int length = text.Length;
+// for (int i = 0; i < length; i++);
+// {
+// if(text[i] ==oldValue) result + $"{NewValue}"
+// else result + $"{str[i]}";
+// }
+// return result;
+// }
+// string newText = replace(text, "", " | ");
+// Console.WriteLine(newText);
+
+// string Replase(string text, char oldValue, char newValue)
+// {
+// string result = String.Empty;
+// int length = text.Length;
+// for (int i = 0; i < length; i++)
+// {
+// if(text[i] == oldValue) result = result + ($"{NewValue}");
+// else result + result $"{text[i]} " ";
+// }
+// return result;
+// }
+
+// string newText = Replace(text, "", " | ");
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(text, "к ", " К ");
+// Console.WriteLine(newText);
+
+
+// int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+// void PtintArray(int[] array)
+// {
+// int count = array.Length;
+// for (int i = 0; i < count; i++)
+// {
+// Console.Write($"{array[i]}");
+// }
+// Console.WriteLine();
+// }
+// PrintArray(arr);
+
+
+
+
+// int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+// void PtintArray(int[] array)
+// {
+// int count = array.Length
+// for (int i = 0; i < count; i++)
+// {
+// Console.Write($”{array[i]}”);
+// }
+// Console.WriteLine();
+// }
+// void selectionSort(int[] array)
+// {
+// for (int i = 0; i < array.Length - 1; i++)
+// {
+// int minPosition = i;
+// for (int j = i+1; j < array.Length; j++)
+// {
+// if(array[j] < array[minPosition]) minPosition = j;
+// {
+// minPosition = j
+// }
+// }
+// int temporary = array[i]
+// array[i] = array[minPosition];
+// array[minPosition] = temporary;
+// }
+// }
+// PrintArray(arr);
+// SelectionSort(arr);
+// PrintArray(arr);
---
+++ С#4lessonseminar/Program.cs
@@ -0,0 +1,90 @@
+﻿// 1
+// int summa(int num)
+// {
+//     int res=0;
+//     for (int i=1; i<=num; i++) {
+//     res = res + i;
+//     }
+//     return res;
+// }
+
+// try
+// {
+// System.Console.WriteLine("Введите натуральное число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine($"Сумма всех чисел от 1 до {num} равна {summa(num)}");
+// }
+// catch
+// {
+//     System.Console.WriteLine("Надо вводить именно целое число!!!");
+// }
+
+
+//2
+// int Result(int x)
+// {
+//     int count = 0;
+//     while (x > 0)
+//     {
+//         x = x / 10;
+//         count++;
+//     }
+//     return count;
+// }
+
+// System.Console.WriteLine("Введите любое число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Колличество цифр в числе: {Result(num)}");
+
+
+
+//3
+
+// int Resault(int num)
+// {
+//     int result = 1;
+//     for (int i = 1; i <= num; i++)
+//     {
+//         result *= i;
+//     }
+//     return result;
+// }
+
+// System.Console.WriteLine("Введите любое число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Факториал числа: {Resault(num)}");
+
+
+// 4
+// int[] CreateArray(int n)
+// {
+//     Random random = new Random();
+//     int[] numbers = new int[n];
+//     System.Console.Write("Массив:");
+//     for (int x = 0; x < numbers.Length; x++)
+//     {
+//         numbers[x] = random.Next(0, 2);
+//     }
+//     return numbers;
+// }
+
+// void PrintArray(int [] numbers)
+// {
+//     for (int x = 0; x < numbers.Length; x++)
+//     System.Console.Write($" {numbers[x]}");
+//     Console.WriteLine();
+// }
+
+// double Result(int [] numbers)
+// {
+//     double summ = 0;
+//     for (int x = 0; x < numbers.Length; x++)
+//     {
+//         summ += numbers[x];
+//     }
+//     return summ / numbers.Length;
+// } 
+
+// int [] numbers = CreateArray(8);
+// PrintArray(numbers);
+// Console.WriteLine($"Среднне врифмитическое массива: {Result(numbers)}");
